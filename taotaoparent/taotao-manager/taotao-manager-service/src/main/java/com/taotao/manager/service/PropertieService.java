a4445dc523b12579bee4cf28ffc561cb87bd2655
xieegai <xieegai@163.com>
2018-11-03 15:04:17 +0800

    图片上传后台代码

---
+++ taotaoparent/taotao-manager/taotao-manager-service/src/main/java/com/taotao/manager/service/PropertieService.java
@@ -0,0 +1,17 @@
+package com.taotao.manager.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zjj
+ * @date 18-11-3 14:56
+ */
+@Service
+public class PropertieService {
+
+    public static final String IMAGE_BASE_URL = "http://127.0.0.1/";
+
+    public static final String REPOSITORY_PATH = "D://upload";
+
+
+}
